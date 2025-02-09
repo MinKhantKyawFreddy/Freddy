16775a5f2314dac1d5e120b106c87e773a536a4e
minkhantkyaw <minkhantkyaw@my.jcu.edu.au>
2025-02-09 11:49:15 +0800

    answer questions and write a code to produce a random number between 1 and 100

---
+++ prac_03/randoms.py
@@ -0,0 +1,18 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+
+# I saw 18 on line 1.
+# The smallest number that I could have seen was 5 and the largest number is 20.
+
+# I saw 3 on line 2.
+# The smallest number that I could have seen is 3 and the largest number is 9.
+
+# I saw 4.514498551560489 on line 3.
+# The smallest number that i could have seen is 2.5 and the largest number is 5.5.
+
+# Code to produce a random number between 1 and 100 inclusive:
+print(random.randint(1,100))
